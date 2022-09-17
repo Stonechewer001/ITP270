02c7b675bf9e4acdd08bb4b005a6bf06893c11cc
Stonechewer001 <stonemorder@gmail.com>
2022-09-17 16:30:43 -0400

    Adding the modified ping_sweep file

--- ping_sweep.sh
+++ ping_sweep.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-echo "ENTER the Subnet:"
+echo "Enter the Subnet:"
 read Subnet
 
 if [ "$Subnet" == "" ]
 then
-	echo "ENTER the Subnet:"
-	echo "Syntax Example = ./pingsweep,sh < 10.1.161"
+	echo "Enter the Subnet:"
+	echo "Syntax Example = ./ping_sweep,sh 10.1.161"
 else
 
-for IP in $(seq 1 254); do
+	for IP in $(seq 1 254); do
 
-	ping -c 1 $Subnet.$IP | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+		ping -c 1 $Subnet.$IP | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> discoveredIPs.txt &
 done
 fi
+
+
+cat discoveredIPs.txt | sort > discoveredIPs.txt
+
+nmap -T4 -sV -A -iL discoveredIPs.txt -oN PortScanIps.txt --append-output
+
+exit
