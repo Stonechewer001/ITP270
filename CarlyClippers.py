a52dac29897b6b4c794a7373b4244ff848065ef8
Stonechewer001 <stonemorder@gmail.com>
2022-11-05 13:12:24 -0400

    added CarlyClippers.py

---
+++ CarlyClippers.py
@@ -0,0 +1,24 @@
+#!/bin/python
+
+hairstyles = ["afro", "bowl", "dreadlocks", "bald", "flowcut", "lowcut", "waves", "mohawk"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
+for i in prices:
+	total_price += i
+average_price = total_price/len(prices)
+print('Average Haircut Price: ' + str(average_price))
+new_prices = [i - 5 for i in prices]
+print(new_prices)
+
+total_revenue = 0
+for i in range(len(prices)):
+	total_revenue += prices[i]*last_week[i]
+print('Total Revenue:'+str(total_revenue))
+average_daily_revenue = total_revenue/7
+print(average_daily_revenue)
+
+cuts_under_30 = [i for i in new_prices if i < 30]
+print(cuts_under_30)
